a8e6e55342f5324cef0e6f06c4ab84e4b1903332
rochardEloi <rochardeliamar.eloi@uniq.edu>
2020-09-22 13:02:21 -0400

    Add Route for Stuff

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Thing = require("./models/thing");
+const stuffRoutes = require("./routes/stuff");
 const app = express();
 /* mongoose.connect('mongodb+srv://rochard_database:127.0.0.1:27017/?gssapiServiceName=mongodb', {
         useNewUrlParser: true,
@@ -23,62 +23,6 @@ app.use((req, res, next) => {
     next();
 });
 app.use(bodyParser.json());
+app.use("./api/stuff", stuffRoutes);
 
-app.post('/api/stuff', (req, res, next) => {
-    let name = req.body;
-    console.log(name.title);
-    console.log(req.body);
-    delete req.body._id;
-    const thing = new Thing({
-        //title : name.title;
-        ...req.body
-    });
-    thing.save()
-        .then(() => res.status('201').json({ message: 'Enregistrement effectuer' }))
-        .catch(error => res.status('400').json({ error }))
-})
-
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status('200').json(thing))
-        .catch(error => res.status('400').json(error))
-})
-app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id })
-        .then(() => res.status('200').json({ message: "OK" }))
-        .catch(error => res.status('400').json(error))
-
-
-})
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-        .then(() => res.status('200').json({ message: "OK" }))
-        .catch(error => res.status('400').json(error))
-
-});
-app.get('/api/stuff', (req, res, next) => {
-
-    Thing.find()
-        .then(things => res.status('200').json(things))
-        .catch(error => res.status('400').json(error))
-        /* const stuff = [{
-                _id: 'oeihfzeoi',
-                title: 'Mon premier objet',
-                description: 'Les infos de mon premier objet',
-                imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-                price: 4900,
-                userId: 'qsomihvqios',
-            },
-            {
-                _id: 'oeihfzeomoihi',
-                title: 'Mon deuxième objet',
-                description: 'Les infos de mon deuxième objet',
-                imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-                price: 2900,
-                userId: 'qsomihvqios',
-            },
-        ];
-        res.status(200).json(stuff); */
-});
 module.exports = app;
